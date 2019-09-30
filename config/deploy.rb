e85217391ffc535ffae45ec695e4f1381f132553
akane.simauma <daphne.78216simaoki@gmail.com>
2019-09-30 16:38:49 +0900

    chenge  deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,17 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
-set :application, "my_app_name"
+set :application, "chat-space""
 set :repo_url, "git@github.com:p729c2e5aT/chat-space.git"
 
-# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
-# どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/chat-.pem']
 
-# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 # Unicornの設定ファイルの場所
